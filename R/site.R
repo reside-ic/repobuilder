5babc5d040ed6980719f43eed86695bdeb8f159a
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-08-31 20:57:50 +0100

    Fix deploy

--- R/site.R
+++ R/site.R
@@ -1,7 +1,11 @@
 update_site <- function(workdir, dest) {
+  path <- "."
   if (has_gh_pages(path)) {
     gert::git_branch_create("gh-pages", "origin/gh-pages", FALSE, path)
     gert::git_clone(path, dest, "gh-pages")
+  } else {
+    gert::git_init(dest)
+    system2("git", c("-C", dest, "checkout", "--orphan", "gh-pages"))
   }
   init_repo(file.path(path, dest))
 
@@ -41,8 +45,8 @@ update_site <- function(workdir, dest) {
             vcapply(dat, "[[", "version"),
             substr(vcapply(dat, "[[", "sha256"), 1, 7)))
 
-  gert::git_add(dest)
-  gert::git_commit(dest)
+  gert::git_add(".", repo = dest)
+  gert::git_commit(paste(msg, collapse = "\n"), repo = dest)
 }
 
 
@@ -62,4 +66,40 @@ contrib_url <- function(path, type, version) {
 
 has_gh_pages <- function(repo = ".") {
   "origin/gh-pages" %in% gert::git_branch_list(repo)$name
+}
+
+
+init_repo <- function(path) {
+  ## TODO: Hit metacran or something here to get the current R version
+  ## so that this can be bumped.  3.5 should last for the next year or
+  ## so though.
+  dir_create(contrib_url(path, "source"))
+
+  platforms <- c("windows", "macosx", "macosx/mavericks",
+                     "macosx/el-capitan")
+  versions <- c("3.1", "3.2", "3.3", "3.4", "3.5", "3.6", "4.0")
+
+  for (platform in platforms) {
+    for (version in versions) {
+      p <- file.path(path, "bin", platform, "contrib", version)
+      pp <- file.path(p, "PACKAGES")
+      ppz <- paste0(pp, ".gz")
+      dir_create(p)
+      if (!file.exists(pp)) {
+        writeLines(character(0), pp)
+      }
+      if (!file.exists(ppz)) {
+        writeLines_gz(character(0), ppz)
+      }
+    }
+  }
+
+  path
+}
+
+
+writeLines_gz <- function(text, filename, ...) {
+  con <- gzfile(filename)
+  on.exit(close(con))
+  writeLines(text, con, ...)
 }
